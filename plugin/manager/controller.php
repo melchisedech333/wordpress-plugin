58e75c79bb766a86967a24f60811097ab1d1c1f4
melchisedech333 <melchisedech333@gmail.com>
2022-10-05 16:50:04 -0300

    Updates

---
+++ plugin/manager/controller.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Add my new menu to the Admin Control Panel
+ */
+
+add_action('admin_menu', 'verbum_add_admin_menu');
+
+function verbum_add_admin_menu ()
+{
+    add_menu_page(
+        'Example',                      // Title of the page.
+        'Example',                      // Text to show on the menu link.
+        'manage_options',               // Capability requirement to see the link.
+        'example',                      // The 'slug' - file to display when clicking the link.
+        'process_manager_viewer',
+        'dashicons-chart-pie',
+        1
+    );
+}
+
+function process_manager_viewer ()
+{
+    ?>
+    Iesus Salvator s2
+    <?php
+}
+
+
---
+++ plugin/plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin Name: Example
+    Description: Example de plugin.
+    Author: Melchisedech333
+*/
+
+require_once plugin_dir_path(__FILE__) . 'manager/controller.php';
+
+
